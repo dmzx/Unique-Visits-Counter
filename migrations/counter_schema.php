a7237ed9a52232fc542762faeb8bb9c0950b8545
dmzx <info@dmzx-web.net>
2015-09-29 14:03:42 +0200

    Update

--- migrations/counter_schema.php
+++ migrations/counter_schema.php
@@ -26,7 +26,7 @@ class counter_schema extends \phpbb\db\migration\migration
 			'add_tables'	=> array(
 				$this->table_prefix . 'visits_counter'	=> array(
 					'COLUMNS'	=> array(
-						'uvc_ip'		=> array('VCHAR:15', ''),
+						'uvc_ip'		=> array('VCHAR:40', ''),
 						'uvc_timestamp'	=> array('INT:30', 0),
 					),
 					'KEYS'			=> array(
